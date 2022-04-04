5766c22ab4815e62139f40d7f65c696ee1c7d7e0
Santiago <Martnez Bota>
2022-04-04 17:20:38 +0200

    TODO PROMISES TO ALL

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -45,20 +45,18 @@ const updateById = async (req, res) => {
 };
 
 const create = async (req, res) => {
-  try {
-    const userToCreate = req.body;
-    // firmar la contraseña con salt de 10
-    const salt = await bcrypt.genSalt(10);
-
-    userToCreate.password = await bcrypt.hash(userToCreate.password, salt);
-
-    await User.create(userToCreate);
-    return res.status(200).send({
-      message: `${userToCreate.name} ha sido cread@`,
+  
+  const userToCreate = req.body;
+  bcrypt.genSalt(10).then(salt => {
+    bcrypt.hash(userToCreate.password, salt).then(hashedPaswd => {
+      userToCreate.password = hashedPaswd;
+      User.create(userToCreate).then((userCreated) => { 
+        return res.status(200).send({
+          message: `${userCreated.name} ha sido cread@`,
+        });
+      });
     });
-  } catch (err) {
-    handleError(401, 'No se ha podido postear usuario', res);
-  }
+  });
 };
 
 const deleteById = async (req, res) => {
@@ -72,7 +70,7 @@ const deleteById = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {
+const login = (req, res) => {
   if (!req.body.email || !req.body.password) {
     handleError(400.1, 'Parámetros incorrectos', res);
   } else {
@@ -80,32 +78,32 @@ const login = async (req, res) => {
     // comparar contraseñas con bcrypt
     // si es ok, firmar jwt
     // devolver user, userRole, jwt y expiryDate
-
-    const user = await User.findOne({ email: req.body.email }).catch((err) =>
-      console.warn(err)
-    );
-
-    if (user) {
-      const validPass = await bcrypt
-        .compare(req.body.password, user.password)
-        .catch((err) => console.warn(err));
-
-      if (validPass) {
-        const token = jwt.sign({ user }, app.get('masterKey'), {
-          expiresIn: EXPIRE_DATE,
-        });
-        res.status(200).send({
-          user: user.name,
-          role: user.userRoleId,
-          token: token,
-          expiryDate: dayjs().format('DD/MM/YYYY hh:mm A'),
-        });
-      } else {
-        handleError(401.1, 'Contraseña incorrecta', res);
-      }
-    } else {
-      handleError(404, 'Usuario no encontrado', res);
-    }
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          bcrypt
+            .compare(req.body.password, user.password)
+            .then((pass) => {
+              if (pass) {
+                const token = jwt.sign({ user }, app.get('masterKey'), {
+                  expiresIn: EXPIRE_DATE,
+                });
+                res.status(200).send({
+                  user: user.name,
+                  role: user.userRoleId,
+                  token: token,
+                  expiryDate: dayjs().format('DD/MM/YYYY hh:mm A'),
+                });
+              } else {
+                handleError(401.1, 'Contraseña incorrecta', res);
+              }
+            })
+            .catch(() => handleError(404, 'Usuario no encontrado', res));
+        }
+      })
+      .catch(() => {
+        handleError(404, 'Usuario no encontrado', res); // todo fill
+      });
   }
 };
 
