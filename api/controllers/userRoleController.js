2ec00b1794d3b89ba11b17c8a2dcd9dcf82f8131
Santiago <Martnez Bota>
2022-04-04 16:07:24 +0200

    comentarios borrados

--- api/controllers/userRoleController.js
+++ api/controllers/userRoleController.js
@@ -19,41 +19,6 @@ const findId = async (req, res) => {
   }
 };
 
-// const updateById = async (req, res) => {
-//   try {
-//     await UserRole.findOneAndUpdate({ _id: req.params.id }, req.body);
-//     return res
-//       .status(200)
-//       .send({ message: `Rol de usuario actualizado: ${JSON.stringify(req.body)}` });
-//   } catch (err) {
-//     detectedError(err, res);
-//   }
-// };
-
-// const create = async (req, res) => {
-//   try {
-//     let insert = {};
-//     insert = req.body;
-//     await UserRole.create(insert);
-//     return res
-//       .status(200)
-//       .send({ message: `Rol de usuario creado: ${JSON.stringify(insert.name)}` });
-//   } catch (err) {
-//     res.status(500).send({ error: "No se ha podido postear Rol de usuario" });
-//   }
-// };
-
-// const deleteById = async (req, res) => {
-//   try {
-//     await UserRole.deleteOne({ _id: req.params.id });
-//     return res
-//       .status(200)
-//       .send({ message: `Rol de usuario borrado con id: ${req.params.id}` });
-//   } catch (err) {
-//     detectedError(err, res);
-//   }
-// };
-
 export default{
   getAll,
   findId,
