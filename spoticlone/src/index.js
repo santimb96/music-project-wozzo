cc378c44ed37a2cd8165816e41a2c143395c2676
Santiago <Martnez Bota>
2022-04-05 14:08:47 +0200

    run watch en custom.css

--- spoticlone/src/index.js
+++ spoticlone/src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
