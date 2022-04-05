a2c8db977714cf3c2f7b7be21e1a2b823acbe41b
Santiago <Martnez Bota>
2022-04-05 08:36:54 +0200

    song y userRole controller refactoring async to promises

--- api/controllers/songController.js
+++ api/controllers/songController.js
@@ -2,54 +2,40 @@ import Song from '../models/song.js';
 import handleError from './errorController.js';
 
 const getAll = async (req, res) => {
-  try {
-    const songs = await Song.find({});
-    res.status(200).send({songs});    
-  } catch (err) {
-    handleError(err, 'Canciones no encontradas', res);
-  }
+  Song.find({})
+    .then(songs => res.status(200).send({songs}))
+    .catch(() => handleError(404, 'Canciones no encontradas', res));
 };
 
 const findId = async (req, res) => {
-  try {
-    const song = await Song.findOne({ _id: req.params.id });
-    song? res.status(200).send({ song }): handleError(404, 'Canción no encontrada', res);
-  } catch (err) {
-    handleError(404, 'Canción no encontrada', res);
-  }
+  Song.findOne({ _id: req.params.id })
+    .then(song => song? res.status(200).send({ song }): handleError(404, 'Canción no encontrada', res))
+    .catch (() =>handleError(404, 'Canción no encontrada', res));
 };
 const updateById = async (req, res) => {
-  try{
-    const song = await Song.findOneAndUpdate({ _id: req.params.id }, req.body);
-    return res
+  Song.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(song => res
       .status(200)
-      .send({ message: `${song.name} se ha actualizado` });
-  } catch (err) {
-    handleError(404, 'No se ha podido actualizar la canción', res);
-  }
+      .send({ message: `${song.name} se ha actualizado` }))
+    .catch(() => handleError(404, 'No se ha podido actualizar la canción', res));
 };
 
 const create = async (req, res) => {
-  try {
-    const songToCreate = req.body;
-    await Song.create(songToCreate);
-    return res
+  const songToCreate = req.body;
+
+  Song.create(songToCreate)
+    .then(song => res
       .status(200)
-      .send({ message: `${songToCreate.name} ha sido cread@` });
-  } catch (err) {
-    handleError(401, 'No se ha podido postear la canción', res);
-  }
+      .send({ message: `${song.name} ha sido cread@` }))
+    .catch(() => handleError(401, 'No se ha podido postear la canción', res));
 };
 
 const deleteById = async (req, res) => {
-  try {
-    await Song.deleteOne({ _id: req.params.id });
-    return res
+  Song.findOneAndDelete({ _id: req.params.id })
+    .then(() => res
       .status(200)
-      .send({ status: 200, message: 'Registro borrado con éxito!' });      
-  } catch (err) {
-    handleError(404, 'No se ha podido borrar la canciñon', res);
-  }
+      .send({ status: 200, message: 'Registro borrado con éxito!' }))
+    .catch(() => handleError(404, 'No se ha podido borrar la canciñon', res));
 };
 
 export default {
--- api/controllers/userRoleController.js
+++ api/controllers/userRoleController.js
@@ -2,27 +2,18 @@ import UserRole from '../models/userRole.js';
 import handleError from './errorController.js';
 
 const getAll = async (req, res) => {
-  try {
-    const userRoles = await UserRole.find({});
-    res.status(200).send({userRoles});    
-  } catch (err) {
-    handleError(err, 'Roles no encontrados', res);
-  }
+  UserRole.find({})
+    .then(userRoles => res.status(200).send({userRoles}))
+    .catch(() => handleError(404, 'Roles no encontrados', res));
 };
 
 const findId = async (req, res) => {
-  try {
-    const userRole = await UserRole.findOne({ _id: req.params.id });
-    userRole ? res.status(200).send({ userRole }):handleError(404, 'Rol no encontrado', res); 
-  } catch (err) {
-    handleError(404, 'Rol no encontrado', res);
-  }
+  UserRole.findOne({ _id: req.params.id })
+    .then(userRole => userRole ? res.status(200).send({ userRole }):handleError(404, 'Rol no encontrado', res))
+    .catch(() => handleError(404, 'Rol no encontrado', res));
 };
 
 export default{
   getAll,
   findId,
-  // updateById,
-  // create,
-  // deleteById,
 };
