91047ab6a5aeb6510cb219f9afee376c14b0e90c
Santiago <Martnez Bota>
2022-04-06 11:21:32 +0200

    ruta registro añadida

--- spoticlone/src/routes/AppRoutes.js
+++ spoticlone/src/routes/AppRoutes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import AdminBackoffice from "../pages/AdminBackoffice";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/backoffice/admin" element={<AdminBackoffice />} />
     </Routes>
   )
