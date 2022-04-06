4bd9948d22783dab60c79a898dbd4e7d0ad94c03
Santiago <Martnez Bota>
2022-04-06 11:49:52 +0200

    refactor public register para el usuario nuevo

--- spoticlone/src/services/user.js
+++ spoticlone/src/services/user.js
@@ -1,10 +1,11 @@
 //import React from "react";
+import { reject } from "bcrypt/promises";
 import { BASE_URI_USER } from "../urls/userUrls";
 
 const login = (email, password) => new Promise((resolve, reject) => {
   if(!email || !password) {
     reject('Error de parámetros');
-    
+
   } else {
     fetch(`${BASE_URI_USER}/public/login`, {
       method: 'POST',
@@ -19,6 +20,15 @@ const login = (email, password) => new Promise((resolve, reject) => {
     .then(response => resolve(response.json()))
     .catch(err => reject(err))
   }
+});
+
+const register = (name, email, password) => new Promise((resolve, reject) => {
+
+  if(!name || !email || !password){
+    reject('Error de parámetros');
+  } else {
+    fetch
+  } 
 });
 
 export {
