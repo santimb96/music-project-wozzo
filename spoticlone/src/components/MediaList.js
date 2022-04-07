d6e3d21b5ccc5343ca0d2a185b93ebf4ee1bf317
Santiago <Martnez Bota>
2022-04-07 12:03:15 +0200

    onClick img media

--- spoticlone/src/components/MediaList.js
+++ spoticlone/src/components/MediaList.js
@@ -10,23 +10,24 @@ const MediaList = () => {
       .then((song) => {
         mediaContext.setSong(song);
       })
-      .catch((err) => console.warn("No se ha podido hacer fecth"));
+      .catch((err) => console.warn(err));
   }, []);
-  console.log(mediaContext.song);
 
-  const randomImg = (idx) => `https://unsplash.it/150/200?image=${idx}`;
-
+  const onClicked = (song) => {
+    console.log(song);
+  }
+  
   return (
     <div className="container-fluid bg-dark text-light">
       <div className="row">
         {mediaContext.song?.songs?.map((song, idx) => (
-          <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-center mt-4">
+          <div  onClick={() => onClicked(song)} className="col-12 col-md-6 col-lg-4 d-flex justify-content-center mt-4">
             <div
               className="card text-light bg-transparent"
               style={{ width: "18rem" }}
             >
               <img
-                src={randomImg(idx)}
+                src="https://i.pinimg.com/736x/00/a1/3c/00a13cf897548091f4042cba761ef00d--cd-cover-dance-music.jpg"
                 className="card-img-top"
                 alt={song.name}
                 style={{ maxWidth: "18rem" }}
