9c6fca68e54671df3acc7f7ef3f5f085f55bd980
Santiago <Martnez Bota>
2022-05-06 09:04:45 +0200

    fav controller done(?)

---
+++ api/controllers/favouriteSongController.js
@@ -0,0 +1,35 @@
+import FavouriteSong from '../models/favouriteSong.js';
+import handleError from './errorController.js';
+
+const getAll = async (req, res) => {
+  FavouriteSong.find({})
+    .then((favouriteSong) => res.status(200).send({ favouriteSong }))
+    .catch(() =>
+      handleError(404, 'No se ha podido obtener ningún usuario', res)
+    );
+};
+
+const create = async (req, res) => {
+  const FavouriteSongToCreate = req.body;
+  FavouriteSong.create(FavouriteSongToCreate)
+    .then(favouriteSong => res
+      .status(201)
+      .send({ status: 201, message: `Se ha creado a ${favouriteSong._id}` }))
+    .catch(() =>  handleError(401, 'No se ha podido postear el favorito', res));
+};
+
+const deleteById = async (req, res) => {
+  FavouriteSong.findOneAndDelete({ _id: req.params.id })
+    .then(() => res
+      .status(200)
+      .send({ status: 200, message: 'Registro borrados con éxito!' }))
+    .catch(() => handleError(404, 'No se ha podido borrar el favorito', res));
+};
+ 
+
+
+export default {
+  getAll,
+  create,
+  deleteById
+};
