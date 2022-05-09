02414866ce43f6ca96982f5ff10d965ed16ea437
Santiago <Martnez Bota>
2022-05-09 09:52:04 +0200

    auth modal refactored

--- spoticlone/src/components/AuthModal/AuthModal.js
+++ spoticlone/src/components/AuthModal/AuthModal.js
@@ -9,7 +9,7 @@ import {
   checkEmail,
   checkEmailOnRegister,
   checkPassword,
-  checkPasswordLength
+  checkPasswordLength,
 } from "../../utils/validators.js";
 import CloseIcon from "@mui/icons-material/Close";
 import AuthContext, { MODAL_STATES } from "../../contexts/AuthContext";
@@ -17,10 +17,16 @@ import { useNavigate } from "react-router-dom";
 import { login, register } from "../../services/user";
 import "./index.scss";
 
-
 const AuthModal = () => {
   // USER CONTEXT
-  const { showAuthModal, setAuthModalType, setUser, setUserRole, authModalType, setShowAuthModal } = useContext(AuthContext);
+  const {
+    showAuthModal,
+    setAuthModalType,
+    setUser,
+    setUserRole,
+    authModalType,
+    setShowAuthModal,
+  } = useContext(AuthContext);
   // NAVIGATE
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
@@ -35,20 +41,18 @@ const AuthModal = () => {
 
   const [errors, setErrors] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  // const [errorSnack, setErrorSnack] = useState(false);
 
   //CLOSE FORM
 
   const handleCloseForm = () => {
     setShowAuthModal(false);
     setErrors(false);
-  }
+  };
 
   const changeModalType = (type) => {
     setErrors(false);
     setAuthModalType(type);
-  }
-  // const handleErrorClose = () => setErrorSnack(false);
+  };
 
   //LOGIN SYSTEM
 
@@ -64,7 +68,7 @@ const AuthModal = () => {
           localStorage.setItem("token", user.token);
           localStorage.setItem("expiryDate", user.expiryDate);
           setShowAuthModal(false);
-          if(user.role === "admin"){
+          if (user.role === "admin") {
             navigate("/backoffice/roles");
           }
         })
@@ -95,7 +99,7 @@ const AuthModal = () => {
           setLoading(false);
           setShowAuthModal(false);
         })
-        .catch(() => {          
+        .catch(() => {
           setLoading(false);
           setErrorMessage("Algo ha ido mal. Vuelve a intentarlo!");
         });
@@ -145,153 +149,157 @@ const AuthModal = () => {
 
   return (
     <div>
-    <Modal
-      open={showAuthModal}
-      onClose={handleCloseForm}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-      disableEnforceFocus
-    >
-      <Box className="modal-delete">
-        <div onClick={handleCloseForm} className="d-flex justify-content-end">
-          <button
-            {...(loading ? { disabled: true } : {})}
-            className="close-modal-button"
-          >
-            <CloseIcon />
-          </button>
-        </div>
-        { errorMessage && (
-          <div className="text-danger justify-content-center">
-            <p className="text-center">{errorMessage}</p>
+      <Modal
+        open={showAuthModal}
+        onClose={handleCloseForm}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        disableEnforceFocus
+      >
+        <Box className="modal-delete">
+          <div onClick={handleCloseForm} className="d-flex justify-content-end">
+            <button
+              {...(loading ? { disabled: true } : {})}
+              className="close-modal-button"
+            >
+              <CloseIcon />
+            </button>
           </div>
-        )}
-        <div className="d-flex justify-content-around m-2">
-          <button
-            onClick={() => changeModalType(MODAL_STATES.LOGIN)}
-            className={
-              authModalType === MODAL_STATES.LOGIN
-                ? "btn-modal-form-active mb-2 mt-2"
-                : "btn-modal-auth mb-2 mt-2"
-            }
-            disabled={loading}
-          >
-            Iniciar Sesión
-          </button>
-          <button
-            onClick={() => changeModalType(MODAL_STATES.REGISTER)}
-            className={
-              authModalType === MODAL_STATES.REGISTER
-                ? "btn-modal-form-active mb-2 mt-2"
-                : "btn-modal-auth mb-2 mt-2"
-            }
-            disabled={loading}
-          >
-            Registrarse
-          </button>
-        </div>
-        <div className="d-flex flex-column">
+          {errorMessage && (
+            <div className="text-danger justify-content-center">
+              <p className="text-center">{errorMessage}</p>
+            </div>
+          )}
+          <div className="d-flex justify-content-around m-2">
+            <button
+              onClick={() => changeModalType(MODAL_STATES.LOGIN)}
+              className={
+                authModalType === MODAL_STATES.LOGIN
+                  ? "btn-modal-form-active mb-2 mt-2"
+                  : "btn-modal-auth mb-2 mt-2"
+              }
+              disabled={loading}
+            >
+              Iniciar Sesión
+            </button>
+            <button
+              onClick={() => changeModalType(MODAL_STATES.REGISTER)}
+              className={
+                authModalType === MODAL_STATES.REGISTER
+                  ? "btn-modal-form-active mb-2 mt-2"
+                  : "btn-modal-auth mb-2 mt-2"
+              }
+              disabled={loading}
+            >
+              Registrarse
+            </button>
+          </div>
+          <div className="d-flex flex-column">
+            {authModalType === MODAL_STATES.REGISTER && (
+              <>
+                <label htmlFor="name">Nombre*</label>
+                <TextField
+                  disabled={loading}
+                  value={name}
+                  type="text"
+                  className="input"
+                  id="name"
+                  placeholder="nombre"
+                  onChange={(e) => setName(e.target.value)}
+                  error={errors && name?.length === 0}
+                  helperText={
+                    errors && name?.length === 0 ? EMPTY_FIELD_MESSAGE : " "
+                  }
+                />
+              </>
+            )}
+            <label htmlFor="email">Email*</label>
+            <TextField
+              disabled={loading}
+              autoComplete="off"
+              className="input"
+              type="email"
+              value={email}
+              id="email"
+              variant="outlined"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              error={
+                (errors && email?.length === 0) ||
+                (errors && !checkEmail(email))
+              }
+              helperText={
+                errors && email?.length === 0
+                  ? EMPTY_FIELD_MESSAGE
+                  : errors && !checkEmail(email)
+                  ? EMAIL_NOT_VALID_MESSAGE
+                  : ""
+              }
+            />
+
+            <label htmlFor="password">Contraseña*</label>
+            <TextField
+              disabled={loading}
+              autoComplete="off"
+              className="input"
+              type="password"
+              value={password}
+              id="password"
+              variant="outlined"
+              placeholder="contraseña"
+              onChange={(e) => setPassword(e.target.value)}
+              error={(errors || errors) && password?.length === 0}
+              helperText={
+                errors && password?.length === 0 ? EMPTY_FIELD_MESSAGE : " "
+              }
+            />
+          </div>
+
           {authModalType === MODAL_STATES.REGISTER && (
             <>
-              <label htmlFor="name">Nombre*</label>
+              <label htmlFor="passRepeat">Repite contraseña*</label>
               <TextField
                 disabled={loading}
-                value={name}
-                type="text"
                 className="input"
-                id="name"
-                placeholder="nombre"
-                onChange={(e) => setName(e.target.value)}
-                error={errors && name?.length === 0}
+                type="password"
+                value={passRepeat}
+                id="passRepeat"
+                variant="outlined"
+                placeholder="repite contraseña"
+                onChange={(e) => setPassRepeat(e.target.value)}
+                error={errors && passRepeat?.length === 0}
                 helperText={
-                  errors && name?.length === 0 ? EMPTY_FIELD_MESSAGE : " "
+                  errors && passRepeat?.length === 0 ? EMPTY_FIELD_MESSAGE : ""
                 }
               />
             </>
           )}
-          <label htmlFor="email">Email*</label>
-          <TextField
-            disabled={loading}
-            autoComplete="off"
-            className="input"
-            type="email"
-            value={email}
-            id="email"
-            variant="outlined"
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-            error={
-              ((errors || errors) && email?.length === 0) || ((errors || errors) && !checkEmail(email))
-            }
-            helperText={
-              (errors || errors) && email?.length === 0
-                ? EMPTY_FIELD_MESSAGE
-                : (errors || errors) && !checkEmail(email)
-                ? EMAIL_NOT_VALID_MESSAGE
-                : ""
-            }
-          />
 
-          <label htmlFor="password">Contraseña*</label>
-          <TextField
-            disabled={loading}
-            autoComplete="off"
-            className="input"
-            type="password"
-            value={password}
-            id="password"
-            variant="outlined"
-            placeholder="contraseña"
-            onChange={(e) => setPassword(e.target.value)}
-            error={(errors || errors) && password?.length === 0}
-            helperText={
-              (errors || errors) && password?.length === 0 ? EMPTY_FIELD_MESSAGE : " "
-            }
-          />
-        </div>
-
-        {authModalType === MODAL_STATES.REGISTER && (
-          <>
-            <label htmlFor="passRepeat">Repite contraseña*</label>
-            <TextField
-              disabled={loading}
-              className="input"
-              type="password"
-              value={passRepeat}
-              id="passRepeat"
-              variant="outlined"
-              placeholder="repite contraseña"
-              onChange={(e) => setPassRepeat(e.target.value)}
-              error={errors && passRepeat?.length === 0}
-              helperText={
-                errors && passRepeat?.length === 0 ? EMPTY_FIELD_MESSAGE : ""
-              }
-            />
-          </>
-        )}
-
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          <div className="typo-flex">
-            <button
-              onClick={() => authModalType === MODAL_STATES.LOGIN ? onLogin() : onRegister()}
-              className="auth-buttons"
-              disabled={loading}
-            >
-              {authModalType === MODAL_STATES.LOGIN ? "Iniciar Sesión" : "Registrarse"}
-            </button>
-          </div>
-        </Typography>
-        {loading ? (
-          <Typography className="d-flex justify-content-center ">
-            <SpinnerLoading />
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <div className="typo-flex">
+              <button
+                onClick={() =>
+                  authModalType === MODAL_STATES.LOGIN
+                    ? onLogin()
+                    : onRegister()
+                }
+                className="auth-buttons"
+                disabled={loading}
+              >
+                {authModalType === MODAL_STATES.LOGIN
+                  ? "Iniciar Sesión"
+                  : "Registrarse"}
+              </button>
+            </div>
           </Typography>
-        ) : (
-          ""
-        )}
-        <small className="small-style">*Campos requeridos</small>
-      </Box>
-    </Modal>
-    {/* <SnackBarError open={errorSnack} handleErrorClose={handleErrorClose} /> */}
+          {loading && (
+            <Typography className="d-flex justify-content-center ">
+              <SpinnerLoading />
+            </Typography>
+          )}
+          <small className="small-style">*Campos requeridos</small>
+        </Box>
+      </Modal>
     </div>
   );
 };
