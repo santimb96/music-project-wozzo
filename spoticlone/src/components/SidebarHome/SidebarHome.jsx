a5ccea0b57e527e175f3f727ad34020f456a8455
Santiago <Martnez Bota>
2022-05-09 16:48:38 +0200

    some scss to home and fav pages

--- spoticlone/src/components/SidebarHome/SidebarHome.jsx
+++ spoticlone/src/components/SidebarHome/SidebarHome.jsx
@@ -19,32 +19,21 @@ const SidebarHome = () => {
   };
 
   return (
-    <div className="col-12 col-md-2 bg-dark">
+    <div className="sidebar">
       <div className="row header-responsive">
-        <header className="col-12 pt-3 spotilogo d-flex justify-content-center">  
-            <img onClick={() => handleOpenSidebar()} src={spotiLogo} alt="spoticloneLogo"></img>
+        <header className="col-12 pt-3 spotilogo d-flex justify-content-center">
+          <img
+            onClick={() => handleOpenSidebar()}
+            src={spotiLogo}
+            alt="spoticloneLogo"
+          ></img>
         </header>
-        <Link to="/favourites">Favourites</Link>
+        <div className="sidebar-list">
+
+            <Link to="/favourites">Favourites</Link>
+
+        </div>
       </div>
-{/* 
-      <div ref={sidebarRef} className="row">
-        <div className="col-12 d-flex flex-column">
-        <div class="card" style={{ width: "18rem" }}>
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-        </div>
-      </div> */}
-        
     </div>
   );
 };
