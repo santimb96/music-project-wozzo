55a5505ec4c653dc52a320429a164aa94165ac4d
Santiago <Martnez Bota>
2022-05-19 11:26:59 +0200

    multer added to genres router

--- api/routes/genre.js
+++ api/routes/genre.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import genreController from '../controllers/genreController.js';
+import multer from 'multer';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 
 const router = express.Router();
 
 router
   .get('/', genreController.getAll)
-  .post('/', genreController.create)
+  .post('/',upload.single('genreImg'), genreController.create)
   .get('/:id', genreController.findId)
-  .put('/:id', genreController.updateById)
+  .put('/:id',upload.single('genreImg'), genreController.updateById)
   .delete('/:id', genreController.deleteById);
 
 export default router;
