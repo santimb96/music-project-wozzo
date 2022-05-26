9a2c73ef7863f39e369ac0e59907647e3e236daf
Santiago <Martnez Bota>
2022-05-26 16:10:50 +0200

    button to share the app in twitter added

--- spoticlone/src/components/Footer/Footer.jsx
+++ spoticlone/src/components/Footer/Footer.jsx
@@ -8,9 +8,10 @@ const Footer = () => {
           <h5>Santiago Martínez, {new Date().getFullYear()}.</h5>
         <div className="social-links">
           <div>
-            <a href="https://github.com/santimb96"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/santi-martinez-bota/"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="mailto:santiagomartinezbota@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+            <a href="https://github.com/santimb96"><i className="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/santi-martinez-bota/"><i className="fa-brands fa-linkedin"></i></a>
+            <a href="mailto:santiagomartinezbota@gmail.com"><i className="fa-solid fa-envelope"></i></a>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text="Incréible reproductor de música!" data-url="https://music-project-wozzo-santimb96.vercel.app" data-via="@santimb96" data-hashtags="#SpotiClone" data-show-count="false"><i className="fa-brands fa-twitter"></i></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
           </div>
         </div>
       </div>
